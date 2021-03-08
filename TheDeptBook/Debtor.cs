b72fce382bcd824da79548fd24a6a53d6d7a251d
andersKallesoee <anderskallesoee@gmail.com>
2021-03-08 10:03:14 +0100

    Added class Debtor

---
+++ TheDeptBook/Debtor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDeptBook
+{
+    public class Debtor
+    {
+        private string id;
+        private string name;
+        private double debtValue;
+
+        public Debtor() { }
+
+        public Debtor(string dId, string dName, double dValue)
+        {
+            id = dId;
+            name = dName;
+            debtValue = dValue;
+        }
+
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public double Value
+        {
+            get
+            {
+                return debtValue;
+            }
+            set
+            {
+                debtValue = value;
+            }
+        }
+    }
+}
