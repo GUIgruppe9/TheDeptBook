b9a35294f3f916ec233454713ecfb2fdefd2553e
MarcusKluver1 <marcus.kluver@gmail.com>
2021-03-08 09:41:15 +0100

    opsat window

--- TheDeptBook/MainWindow.xaml.cs
+++ TheDeptBook/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TheDeptBook
         {
             InitializeComponent();
         }
+
+        private void AddDept_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
