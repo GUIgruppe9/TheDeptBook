bec68aa3457d25c6b1f0892ad9cafc8b1ec54bc3
andersKallesoee <anderskallesoee@gmail.com>
2021-03-15 10:46:05 +0100

    Moved Deptor class to folder Model. Added debtors and values to listbox

--- TheDeptBook/Model/Debtor.cs
+++ TheDeptBook/Model/Debtor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheDeptBook
+namespace TheDeptBook.Model
 {
     public class Debtor
     {
--- TheDeptBook/ViewModel/MainWindowViewModel.cs
+++ TheDeptBook/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Mvvm;
+using TheDeptBook.Model;
 
 namespace TheDeptBook.ViewModel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : BindableBase
     {
+        private ObservableCollection<Debtor> debtors;
+
+        public MainWindowViewModel()
+        {
+            debtors = new ObservableCollection<Debtor>
+            {
+                new Debtor("1", "Marcus", 0.1),
+                new Debtor("2", "Mikkel", 500),
+                new Debtor("3", "Anders", 44)
+            };
+        }
+
+        // Properties
+        public ObservableCollection<Debtor> Debtors
+        {
+            get { return debtors; }
+            set { SetProperty(ref debtors, value); }
+        }
+
+        private Debtor currentDebtor = null;
+
+        public Debtor CurrentDebtor
+        {
+            get { return currentDebtor; }
+            set
+            {
+                SetProperty(ref currentDebtor, value);
+            }
+        }
+
+        int currentIndex = -1;
+        public int CurrentIndex
+        {
+            get { return currentIndex; }
+            set
+            {
+                SetProperty(ref currentIndex, value);
+            }
+        }
     }
 }
