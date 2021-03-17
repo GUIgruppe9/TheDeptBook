c0b46ae0185f3ae6ca543d833aeb46b8cc25bfba
andersKallesoee <anderskallesoee@gmail.com>
2021-03-17 15:22:11 +0100

    Added class Debit and added Debits to Debitor. TODO: Show debits in datagrid in DebtorWindow

---
+++ TheDeptBook/Model/Debit.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDeptBook.Model
+{
+    public class Debit
+    {
+        private string id;
+        private DateTime dateTime;
+        private double debitValue;
+
+        public Debit() { }
+
+        public Debit(string i, double val)
+        {
+            id = i;
+            dateTime = DateTime.Now;
+            debitValue = val;
+        }
+
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public DateTime DateTime
+        {
+            get
+            {
+                return dateTime;
+            }
+            set
+            {
+                dateTime = value;
+            }
+        }
+
+        public double DebitValue
+        {
+            get { return debitValue; }
+            set { debitValue = value; }
+        }
+    }
+}
--- TheDeptBook/Model/Debtor.cs
+++ TheDeptBook/Model/Debtor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace TheDeptBook.Model
         private string id;
         private string name;
         private double debtValue;
+        public ObservableCollection<Debit> Debits;
 
         public Debtor() { }
 
