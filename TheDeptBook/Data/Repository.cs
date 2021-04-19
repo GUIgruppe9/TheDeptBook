a87d1249f6dafb09553ea4c901a1c7d32de2a585
andersKallesoee <anderskallesoee@gmail.com>
2021-04-19 09:58:37 +0200

    Tilføjes repository class til brug ved persistering

---
+++ TheDeptBook/Data/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TheDeptBook.Model;
+
+namespace TheDeptBook.Data
+{
+    public class Repository
+    {
+        internal static bool ReadFile(string fileName, out ObservableCollection<Debtor> agents)
+        {
+            // Create an instance of the XmlSerializer class and specify the type of object to deserialize.
+            XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Debtor>));
+
+            TextReader reader = new StreamReader(fileName);
+            // Deserialize all the agents.
+            agents = (ObservableCollection<Debtor>)serializer.Deserialize(reader);
+            reader.Close();
+
+            return true;
+        }
+
+        internal static void SaveFile(string fileName, ObservableCollection<Debtor> agents)
+        {
+            // Create an instance of the XmlSerializer class and specify the type of object to serialize.
+            XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Debtor>));
+            TextWriter writer = new StreamWriter(fileName);
+            // Serialize all the agents.
+            serializer.Serialize(writer, agents);
+            writer.Close();
+        }
+    }
+}
--- TheDeptBook/ViewModel/MainWindowViewModel.cs
+++ TheDeptBook/ViewModel/MainWindowViewModel.cs
@@ -101,5 +101,6 @@ namespace TheDeptBook.ViewModel
                 }));
             }
         }
+
     }
 }
